3d3db13605386b1d9c0fa4b28c1739a11f447f1a
Guilherme <ariel.gmachado@hotmail.com>
2025-08-28 21:17:49 -0300

    feat(model): adicionar enum TipoVeiculo com valores de diária

--- src/model/TipoVeiculo.java
+++ src/model/TipoVeiculo.java
@@ -1,5 +1,17 @@
 package model;
 
-public class TipoVeiculo {
+public enum TipoVeiculo {
+  ELETRICO(150.0),
+  AUTOMATICO(200.0),
+  MANUAL(100.0);
 
+  private final double valorDiaria;
+
+  TipoVeiculo(double valorDiaria) {
+    this.valorDiaria = valorDiaria;
+  }
+
+  public double getValorDiaria() {
+    return valorDiaria;
+  }
 }
