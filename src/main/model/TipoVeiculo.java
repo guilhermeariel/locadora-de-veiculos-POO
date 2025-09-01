9fa8027c257d45044b0eed34f03a92d4e2dfa3b0
Guilherme <ariel.gmachado@hotmail.com>
2025-09-01 20:49:57 -0300

    Altera o tipo viculo

--- src/main/model/TipoVeiculo.java
+++ src/main/model/TipoVeiculo.java
@@ -1,9 +1,9 @@
 package model;
 
 public enum TipoVeiculo {
-  MANUAL(100.0),
-  AUTOMATICO(150.0),
-  ELETRICO(200.0);
+  PEQUENO(100.0),
+  MEDIO(150.0),
+  SUV(200.0);
 
   private final double valorDiaria;
 
