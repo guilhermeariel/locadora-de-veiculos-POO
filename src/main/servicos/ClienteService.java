4c58f05a05bfde6bd403c6571ddb5013a885e055
Paula Eduarda Justino <rpaulaeduarda@gmail.com>
2025-09-03 13:15:27 -0300

    add: implementa interface, impl e testes do ClienteService

---
+++ src/main/servicos/ClienteService.java
@@ -0,0 +1,10 @@
+package servicos;
+
+import model.Cliente;
+
+public interface ClienteService {
+
+    void cadastrarCliente(String nome, String documento, boolean isPessoaFisica);
+    Cliente buscarClientePorId(String documento);
+    void atualizarCliente(String documento, String novoNome);
+}
