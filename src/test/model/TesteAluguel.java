55d94e8311c0a8df9f691047a7d182d92f749829
Guilherme <ariel.gmachado@hotmail.com>
2025-09-08 01:26:14 -0300

    ATT

--- src/test/model/TesteAluguel.java
+++ src/test/model/TesteAluguel.java
@@ -6,8 +6,8 @@ public class TesteAluguel {
     public static void main(String[] args) {
         // Criar veículos válidos
         Veiculo veiculo1 = new Veiculo("ABC1234", TipoVeiculo.SEDAN, "Toyota Corolla", true);
-        Veiculo veiculo2 = new Veiculo("XYZ5678", TipoVeiculo.SEDAN, "Honda Civic", true);
-        Veiculo veiculo3 = new Veiculo("ELE1234", TipoVeiculo.SEDAN, "Tesla Model 3", true);
+        Veiculo veiculo2 = new Veiculo("XYZ5678", TipoVeiculo.HATCH, "Fiat Argo", true);
+        Veiculo veiculo3 = new Veiculo("ELE1234", TipoVeiculo.SUV, "Tesla Model 3", true);
 
         // Criar clientes de teste
         Cliente cliente1 = new PessoaFisica("João", "12345678901");
@@ -15,7 +15,7 @@ public class TesteAluguel {
         Cliente cliente3 = new PessoaFisica("Carlos", "11122233344");
 
         // Criar aluguéis
-        Aluguel aluguel1 = new Aluguel(cliente1, veiculo1, LocalDateTime.now(), LocalDateTime.now().plusDays(5));
+        Aluguel aluguel1 = new Aluguel(cliente1, veiculo1, LocalDateTime.now(), LocalDateTime.now().plusDays(12));
         Aluguel aluguel2 = new Aluguel(cliente2, veiculo2, LocalDateTime.now(), LocalDateTime.now().plusDays(3));
         Aluguel aluguel3 = new Aluguel(cliente3, veiculo3, LocalDateTime.now(), LocalDateTime.now().plusDays(7));
 
