a425fd491b1490d90f5e720a49684e854f9ffd40
Guilherme <ariel.gmachado@hotmail.com>
2025-09-08 00:55:32 -0300

    test: corrigir classe de teste de aluguel com instâncias válidas de veículo, cliente e datas
    - Ajustado o uso correto do enum TipoVeiculo (SEDAN)
    - Corrigido construtor de Veiculo com 4 parâmetros
    - Adicionados clientes fictícios para testes
    - Corrigido construtor de Aluguel com datas simuladas
    - Corrigido método de cálculo de valor do aluguel (calcularValor)

--- src/test/model/TesteAluguel.java
+++ src/test/model/TesteAluguel.java
@@ -1,27 +1,36 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class TesteAluguel {
     public static void main(String[] args) {
-        Veiculo veiculo1 = new Veiculo("ABC1234", "Toyota Corolla", TipoVeiculo.MEDIO);
-        Veiculo veiculo2 = new Veiculo("XYZ5678", "Honda Civic", TipoVeiculo.MEDIO);
-        Veiculo veiculo3 = new Veiculo("ELE1234", "Tesla Model 3", TipoVeiculo.MEDIO);
+        // Criar veículos válidos
+        Veiculo veiculo1 = new Veiculo("ABC1234", TipoVeiculo.SEDAN, "Toyota Corolla", true);
+        Veiculo veiculo2 = new Veiculo("XYZ5678", TipoVeiculo.SEDAN, "Honda Civic", true);
+        Veiculo veiculo3 = new Veiculo("ELE1234", TipoVeiculo.SEDAN, "Tesla Model 3", true);
 
-        Aluguel aluguel1 = new Aluguel(veiculo1, 5);
-        Aluguel aluguel2 = new Aluguel(veiculo2, 3);
-        Aluguel aluguel3 = new Aluguel(veiculo3, 7);
+        // Criar clientes de teste
+        Cliente cliente1 = new PessoaFisica("João", "12345678901");
+        Cliente cliente2 = new PessoaFisica("Maria", "98765432100");
+        Cliente cliente3 = new PessoaFisica("Carlos", "11122233344");
+
+        // Criar aluguéis
+        Aluguel aluguel1 = new Aluguel(cliente1, veiculo1, LocalDateTime.now(), LocalDateTime.now().plusDays(5));
+        Aluguel aluguel2 = new Aluguel(cliente2, veiculo2, LocalDateTime.now(), LocalDateTime.now().plusDays(3));
+        Aluguel aluguel3 = new Aluguel(cliente3, veiculo3, LocalDateTime.now(), LocalDateTime.now().plusDays(7));
 
         System.out.println("Detalhes do Aluguel 1:");
         System.out.println(aluguel1);
-        System.out.println("Valor Total: R$ " + aluguel1.calcularValorTotal());
+        System.out.println("Valor Total: R$ " + aluguel1.calcularValor());
         System.out.println();
 
         System.out.println("Detalhes do Aluguel 2:");
         System.out.println(aluguel2);
-        System.out.println("Valor Total: R$ " + aluguel2.calcularValorTotal());
+        System.out.println("Valor Total: R$ " + aluguel2.calcularValor());
         System.out.println();
 
         System.out.println("Detalhes do Aluguel 3:");
         System.out.println(aluguel3);
-        System.out.println("Valor Total: R$ " + aluguel3.calcularValorTotal());
+        System.out.println("Valor Total: R$ " + aluguel3.calcularValor());
     }
 }
